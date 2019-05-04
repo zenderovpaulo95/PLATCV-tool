d6a603eb01c93a06eae6ff26b35e5dc355406238
Sudakov Pavel <pashok6798@gmail.com>
2019-05-04 08:03:53 +0300

    Some fixes for linux users and add instruction for extract/repack archive

--- PLATCV_tool/Program.cs
+++ PLATCV_tool/Program.cs
@@ -15,38 +15,42 @@ namespace PLATCV_tool
         {
             char slash = Path.DirectorySeparatorChar;
 
-            if ((args.Length == 2 && args[0] == "export" && File.Exists(args[1]))
-            || (args.Length == 3 && args[0] == "export" && File.Exists(args[1]) && Directory.Exists(args[2])))
-            {
-                string InputFile = args[1];
-                string OutputFolder = AppDomain.CurrentDomain.BaseDirectory + "extracted";
-                if (args.Length == 3 && Directory.Exists(args[2])) OutputFolder = args[2];
+			if ((args.Length == 2 && args [0] == "export" && File.Exists (args [1]))
+			             || (args.Length == 3 && args [0] == "export" && File.Exists (args [1]) && Directory.Exists (args [2]))) {
+				string InputFile = args [1];
+				string OutputFolder = AppDomain.CurrentDomain.BaseDirectory + "extracted";
+				if (args.Length == 3 && Directory.Exists (args [2]))
+					OutputFolder = args [2];
 
-                if(File.Exists(InputFile)) WorkWithFiles.ExportFiles(InputFile, OutputFolder, slash);
-            }
-            else if ((args.Length == 2 && args[0] == "import")
-            || (args.Length == 3 && args[0] == "import" && Directory.Exists(args[1])))
-            {
-                string OutputFile = args[1];
-                string InputFolder = AppDomain.CurrentDomain.BaseDirectory + "extracted";
-                if (args.Length == 3 && Directory.Exists(args[1]))
-                {
-                    InputFolder = args[1];
-                    OutputFile = args[2];
-                }
+				if (File.Exists (InputFile))
+					WorkWithFiles.ExportFiles (InputFile, OutputFolder, slash);
+			} else if ((args.Length == 2 && args [0] == "import")
+			                  || (args.Length == 3 && args [0] == "import" && Directory.Exists (args [1]))) {
+				string OutputFile = args [1];
+				string InputFolder = AppDomain.CurrentDomain.BaseDirectory + "extracted";
+				if (args.Length == 3 && Directory.Exists (args [1])) {
+					InputFolder = args [1];
+					OutputFile = args [2];
+				}
 
-                if(Directory.Exists(InputFolder)) WorkWithFiles.ImportFiles(InputFolder, OutputFile, slash);
-            }
-            else if((args.Length == 2 && args[0] == "log" && File.Exists(args[1]))
-                || (args.Length == 3 && args[0] == "log" && File.Exists(args[1])))
-                {
-                    string OutputFile = string.Format(AppDomain.CurrentDomain.BaseDirectory + "{0}table.log", slash);
-                    string InputFile = args[1];
+				if (Directory.Exists (InputFolder))
+					WorkWithFiles.ImportFiles (InputFolder, OutputFile, slash);
+			} else if ((args.Length == 2 && args [0] == "log" && File.Exists (args [1]))
+			                 || (args.Length == 3 && args [0] == "log" && File.Exists (args [1]))) {
+				string OutputFile = string.Format (AppDomain.CurrentDomain.BaseDirectory + "{0}table.log", slash);
+				string InputFile = args [1];
 
-                    if (args.Length == 3) OutputFile = args[2];
+				if (args.Length == 3)
+					OutputFile = args [2];
 
-                    WorkWithFiles.GetTable(InputFile, OutputFile);
-                }
+				WorkWithFiles.GetTable (InputFile, OutputFile);
+			} else 
+			{
+				Console.WriteLine ("How to use tool:");
+				Console.WriteLine ("Extract:\n{0} export \"path{1}to_file.obb\"\nor\n{0} export \"path{1}to_file.obb\" \"path{1}to directory\"\n\n", AppDomain.CurrentDomain.FriendlyName, slash);
+				Console.WriteLine("Import:\n{0} import \"path{1}to directory with resources\" \"path{1}to_file.obb\"\nor\n{0} import \"path{1}to_file\"\n\n", AppDomain.CurrentDomain.FriendlyName, slash);
+				Console.WriteLine ("WARNING: If you don't select a folder, a tool will extract in directory \"{0}{1}extracted\". Import process without selected directory will require same directory \"{0}{1}extracted\"", AppDomain.CurrentDomain.FriendlyName, slash);
+			}
         }
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # PLATCV tool
 A tool for work with archives Professor Laton and the Curious Village on Android and iOS.
 Special thanks to ssh from zenhax forum for this script: https://zenhax.com/viewtopic.php?p=41170#p41170
+
+Build for linux (requires MonoDevelop):
+For linux users: You can try use MonoDevelop (see in you distribution how to install). Try these variants:
+
+1. Build with xbuild: 
+xbuild PLATCV_tool.sln 
+mono 'PLATCV_tool/bin/Release/PLATCV_tool.exe'
+
+2. Build with msbuild: 
+msbuild PLATCV_tool.sln
+mono 'PLATCV_tool/bin/Release/PLATCV_tool.exe'
